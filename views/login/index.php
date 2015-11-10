05c45fb6b58103f7235a5c8b5232d2703f74b0f1
sks40gb <sks.256@gmail.com>
2015-11-10 15:19:00 +0530

    setup notes for other instance

--- views/login/index.php
+++ views/login/index.php
@@ -4,5 +4,5 @@
     
     <label>Login</label><input type="text" name="login" /><br />
     <label>Password</label><input type="password" name="password" /><br />
-    <label></label><input type="submit" />
+    <label></label><input type="Login" />
 </form>
